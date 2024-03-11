2f6fdcbcf3b9e2bf3c2b54c9675272497f6455d1
Tyson <tyson@horsewell.me>
2024-03-12 05:57:16 +0800

    clean data

---
+++ QuickDBD-export.sql
@@ -0,0 +1,15 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Stocks" (
+    "ID" int  NOT NULL PRIMARY KEY,
+    "StockCode" VARCHAR(10) NOT NULL,
+    "Date" Date NOT NULL,
+    "Close" float NOT NULL,
+    "Volume" float NOT NULL,
+    "Open" float NOT NULL,
+    "High" float NOT NULL,
+    "Low" float NOT NULL
+);
+
